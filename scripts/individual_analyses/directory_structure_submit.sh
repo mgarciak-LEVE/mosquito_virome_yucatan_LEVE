bd329cb85b1b07864f2fc7b149811052d9485211
P-arsimony <trooper0829@gmail.com>
2026-06-30 03:56:26 +0100

    Add: scripts for directory structure and its submission. These scripts are part of a test of wrapper scripts.

---
+++ scripts/individual_analyses/directory_structure_submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ~/git_repos/mosquito_virome_pipeline/scripts/individual_analyses/directory_structure_submit.sh
+
+# Set the date
+DATE=$(date +%d_%m_%Y)
+DATE_DIR="${HOME}/lsf_logs/${DATE}"
+
+# Create dated log directory
+mkdir -p "${DATE_DIR}"
+
+# Submit the job with the paths already resolved
+bsub -o "${DATE_DIR}/directory_structure_%J.out" \
+     -e "${DATE_DIR}/directory_structure_%J.err" \
+     -q normal \
+     -n 1 \
+     -M 1000 \
+     -R "select[mem>1000] rusage[mem=1000] span[hosts=1]" \
+     -G team222 \
+     < "${HOME}/git_repos/mosquito_virome_pipeline/scripts/individual_analyses/directory_structure.sh"
